927f8aa88ce26b867f4483ba8cfeddaba397b6d1
huzaim550 <huzaimzulfiqar@gmail.com>
2026-07-26 14:51:11 +0500

    Added some functionality

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ console.log("this is a console log");
 function print() {
     console.log("hello i am print")
 }
+function changeColor() {
+  document.body.style.backgroundColor = "red";
+}   
+function changeColorBlack() {
+  document.body.style.backgroundColor = "black";
+} 
+function colorReset() {
+  document.body.style.backgroundColor = "white";
+}
